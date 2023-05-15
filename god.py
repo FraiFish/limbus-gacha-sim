83105d1fab7ccdefe6b6e3244ee56c98ae986d4d
Frai <133361728+FraiFish@users.noreply.github.com>
2023-05-15 11:00:12 +0800

    changelog at the end

--- god.py
+++ god.py
@@ -12,91 +12,100 @@ class items:
  # intended but it doesn't work FSR therefore nothing for identities also
 class MainSim: 
     # Set up a lot of foundation so i can do a lot more with this later, some things planned:
+    # next: draw until you have specific thing
     # statistics of pulls: etc, how much of a session was EGO, 3 stars, Etc
     # statistics of % drawn of each sinner 
     # GUI which will make it more than a command to be run
-    # actual words instead of just outputting pull results_str 
     # extra line when EGO/000 drops 
     # extra line when 00 drops
     # extra line for duplicates
     # EGO cannot be drawn twice in a session feature
-    # if draw 0 or 00 just make it output "0" or "00"
+    # if draw 0 or 00 just make it output onestar or twostar
     # interact to keep drawing instead of all at once
     # some sort of database??
     # actual luck vs calculated chance
     # draw until you have specific thing
-    # draw once(implemented)
     # target extraction like banners, 000 guaranteed tickets
     # luck with respect to one specific item that you wish for
     # daily 4 pulls
     # ^ I realized that that would need numerous different banner informations other than the standard so unless that can be automated I'm not doing that
     # sort results_str 
-    # put the tenth pull somewhere random(implemented)
+    # put same stuff into vars 
     # code below 
 
     ### big list of everything in the game (probably a better way to do it)
+    # shorten
+    trashtype = "Trash"
+    idtype = "Identity"
+    egotype = "EGO"
+    threestar = "B"
+    twostar = "C"
+    onestar = "D"
+    Zayin = "A-ZAYIN"
+    Teth = "A-TETH"
+    He = "A-HE"
     ### LCB sinners aka 0 stars, this is all pointless crap for flavor
-    LCB_Sinner_YiSang = items("LCB Sinner Yi Sang", "Yi Sang", "0", "Trash")
-    LCB_Sinner_Knows_Everything = items("LCB Sinner Faust", "Faust", "0", "Trash")
-    LCB_Sinner_DQte = items("LCB Sinner Don Quixote", "Don Quixote", "0", "Trash")
-    LCB_Sinner_R_S = items("LCB Sinner Ryoshu", "Ryoshu", "0", "Trash")
-    LCB_Sinner_Meursault = items("LCB Sinner Meursault", "Meursault", "0", "Trash")
-    LCB_Sinner_Hong = items("LCB Sinner Hong Lu", "Hong Lu", "0", "Trash")
-    LCB_Sinner_Heathcliff = items("LCB Sinner Heathcliff", "Heathcliff", "0", "Trash")
-    LCB_Sinner_Ishmael = items("LCB Sinner Ishmael", "Ishmael", "0", "Trash")
-    LCB_Sinner_Rodya = items("LCB Sinner Rodion", "Rodion", "0", "Trash")
-    LCB_Sinner_Shinculairu = items("LCB Sinner Sinclair", "Sinclair", "0", "Trash")
-    LCB_Sinner_Outis = items("LCB Sinner Outis", "Outis", "0", "Trash")
-    LCB_Sinner_Gregor = items("LCB Sinner Gregor", "Gregor", "0", "Trash")
+    LCB_Sinner_YiSang = items("LCB Sinner Yi Sang", "Yi Sang", onestar, trashtype)
+    LCB_Sinner_Knows_Everything = items("LCB Sinner Faust", "Faust", onestar, trashtype)
+    LCB_Sinner_DQte = items("LCB Sinner Don Quixote", "Don Quixote", onestar, trashtype)
+    LCB_Sinner_R_S = items("LCB Sinner Ryoshu", "Ryoshu", onestar, trashtype)
+    LCB_Sinner_Meursault = items("LCB Sinner Meursault", "Meursault", onestar, trashtype)
+    LCB_Sinner_Hong = items("LCB Sinner Hong Lu", "Hong Lu", onestar, trashtype)
+    LCB_Sinner_Heathcliff = items("LCB Sinner Heathcliff", "Heathcliff", onestar, trashtype)
+    LCB_Sinner_Ishmael = items("LCB Sinner Ishmael", "Ishmael", onestar, trashtype)
+    LCB_Sinner_Rodya = items("LCB Sinner Rodion", "Rodion", onestar, trashtype)
+    LCB_Sinner_Shinculairu = items("LCB Sinner Sinclair", "Sinclair", onestar, trashtype)
+    LCB_Sinner_Outis = items("LCB Sinner Outis", "Outis", onestar, trashtype)
+    LCB_Sinner_Gregor = items("LCB Sinner Gregor", "Gregor", onestar, trashtype)
     ### 2 stars, 00s
-    Seven_YiSang = items("Seven Section 6 Yi Sang", "Yi Sang", "00", "Identity")
-    L_Faust = items("Lobotomy Corp. Remnant Faust", "Faust", "00", "Identity")
-    W_Faust = items("W Corp. Cleanup Agent Faust", "Faust", "00", "Identity")
-    Shi_Don = items("Shi Section 5 Director Don Quixote", "Don Quixote", "00", "Identity")
-    N_Don = items("N Corp. Mittelhammer Don Quixote", "Don Quixote", "00", "Identity")
-    Seven_R_S = items("Seven Section 6 Ryoshu", "Ryoshu", "00", "Identity")
-    Liu_Meursault = items("Liu Section 6 Meursault", "Meursault", "00", "Identity")
-    Kuro_Hong = items("Kurokumo Wakashu Hong Lu", "Hong Lu", "00", "Identity")
-    Hong_Liu = items("Liu section 5 Hong Lu", "Hong Lu", "00", "Identity")
-    Shi_Cliff = items("Shi Section 5 Heathcliff", "Heathcliff", "00", "Identity")
-    N_Cliff = items("N Corp. Kleinhammer Heathcliff", "Heathcliff", "00", "Identity")
-    Shi_Mael = items("Shi Section 5 Ishmael", "Ishmael", "00", "Identity")
-    LCCB_Mael = items("LCCB Assistant Manager Ishmael", "Ishmael", "00", "Identity")
-    LCCB_Rodya = items("LCCB Assistant Manager Rodion", "Rodion", "00", "Identity")
-    N_Rodya = items("N Corp. Mittelhammer Rodion", "Rodion", "00", "Identity")
-    Mexiclair = items("Jefe de los Marachis Sinclair", "Sinclair", "00", "Identity")
-    Zweiclair = items("Zwei Section 6 Sinclair", "Sinclair", "00", "Identity")
-    Blade_Outis = items("Blade Lineage Cutthroat Outis", "Outis", "00", "Identity")
-    G_Outis = items("G Corp. Head Manager Outis", "Outis", "00", "Identity")
-    Liu_Gregor = items("Liu Section 6 Gregor", "Gregor", "00", "Identity")
+    Seven_YiSang = items("Seven Section 6 Yi Sang", "Yi Sang", twostar, idtype)
+    L_Faust = items("Lobotomy Corp. Remnant Faust", "Faust", twostar, idtype)
+    W_Faust = items("W Corp. Cleanup Agent Faust", "Faust", twostar, idtype)
+    Shi_Don = items("Shi Section 5 Director Don Quixote", "Don Quixote", twostar, idtype)
+    N_Don = items("N Corp. Mittelhammer Don Quixote", "Don Quixote", twostar, idtype)
+    Seven_R_S = items("Seven Section 6 Ryoshu", "Ryoshu", twostar, idtype)
+    Liu_Meursault = items("Liu Section 6 Meursault", "Meursault", twostar, idtype)
+    Kuro_Hong = items("Kurokumo Wakashu Hong Lu", "Hong Lu", twostar, idtype)
+    Hong_Liu = items("Liu section 5 Hong Lu", "Hong Lu", twostar, idtype)
+    Shi_Cliff = items("Shi Section 5 Heathcliff", "Heathcliff", twostar, idtype)
+    N_Cliff = items("N Corp. Kleinhammer Heathcliff", "Heathcliff", twostar, idtype)
+    Shi_Mael = items("Shi Section 5 Ishmael", "Ishmael", twostar, idtype)
+    LCCB_Mael = items("LCCB Assistant Manager Ishmael", "Ishmael", twostar, idtype)
+    LCCB_Rodya = items("LCCB Assistant Manager Rodion", "Rodion", twostar, idtype)
+    N_Rodya = items("N Corp. Mittelhammer Rodion", "Rodion", twostar, idtype)
+    Mexiclair = items("Jefe de los Marachis Sinclair", "Sinclair", twostar, idtype)
+    Zweiclair = items("Zwei Section 6 Sinclair", "Sinclair", twostar, idtype)
+    Blade_Outis = items("Blade Lineage Cutthroat Outis", "Outis", twostar, idtype)
+    G_Outis = items("G Corp. Head Manager Outis", "Outis", twostar, idtype)
+    Liu_Gregor = items("Liu Section 6 Gregor", "Gregor", twostar, idtype)
     ### 3 stars, 000s
-    V_50 = items("Blade Lineage Salsu Yi Sang", "Yi Sang", "000", "Identity")
-    Grip = items("The One Who Grips Faust", "Faust", "000", "Identity")
-    W_Don = items("W Corp. Cleanup Agent Don Quixote", "Don Quixote", "000", "Identity")
-    Kuro_R_S = items("Kurokumo Wakashu Ryoshu", "Ryoshu", "000", "Identity")
-    Chef_R_S = items("R.B Chef de Cuisine Ryoshu", "Ryoshu", "000", "Identity")
-    W_Meur = items("W Corp. Cleanup Agent Meursault", "Meursault", "000", "Identity")
-    N_Meur = items("N Corp. Großhammer Meursault", "Meursault", "000", "Identity")
-    TT_Lu = items("Tingtang Gangleader Hong Lu", "Hong Lu", "000", "Identity")
-    Rabbit = items("R Corp. 4th Pack Rabbit Heathcliff", "Heathcliff", "000", "Identity")
-    Reindeer = items("R Corp. 4th Pack Reindeer Ishmael", "Ishmael", "000", "Identity")
-    Kuro_Rodya = items("Kurokumo Henchwoman Rodion", "Rodion", "000", "Identity")
-    N_Sinclair = items("The One Who Shall Grip Sinclair", "Sinclair", "000", "Identity")
-    Blade_Sinclair = items("Blade Lineage Salsu Sinclair", "Sinclair", "000", "Identity")
-    Seven_Outis = items("Seven Section 6 Director Outis", "Outis", "000", "Identity")
-    G_regor = items("G Corp. Manager Corporal Gregor", "Gregor", "000", "Identity")
+    V_50 = items("Blade Lineage Salsu Yi Sang", "Yi Sang", threestar, idtype)
+    Grip = items("The One Who Grips Faust", "Faust", threestar, idtype)
+    W_Don = items("W Corp. Cleanup Agent Don Quixote", "Don Quixote", threestar, idtype)
+    Kuro_R_S = items("Kurokumo Wakashu Ryoshu", "Ryoshu", threestar, idtype)
+    Chef_R_S = items("R.B Chef de Cuisine Ryoshu", "Ryoshu", threestar, idtype)
+    W_Meur = items("W Corp. Cleanup Agent Meursault", "Meursault", threestar, idtype)
+    N_Meur = items("N Corp. Großhammer Meursault", "Meursault", threestar, idtype)
+    TT_Lu = items("Tingtang Gangleader Hong Lu", "Hong Lu", threestar, idtype)
+    Rabbit = items("R Corp. 4th Pack Rabbit Heathcliff", "Heathcliff", threestar, idtype)
+    Reindeer = items("R Corp. 4th Pack Reindeer Ishmael", "Ishmael", threestar, idtype)
+    Kuro_Rodya = items("Kurokumo Henchwoman Rodion", "Rodion", threestar, idtype)
+    N_Sinclair = items("The One Who Shall Grip Sinclair", "Sinclair", threestar, idtype)
+    Blade_Sinclair = items("Blade Lineage Salsu Sinclair", "Sinclair", threestar, idtype)
+    Seven_Outis = items("Seven Section 6 Director Outis", "Outis", threestar, idtype)
+    G_regor = items("G Corp. Manager Corporal Gregor", "Gregor", threestar, idtype)
     ### Obtainable EGOs
-    Rocks = items("Wishing Cairn - Yi Sang", "Yi Sang", "TETH", "EGO")
+    Rocks = items("Wishing Cairn - Yi Sang", "Yi Sang", Teth, egotype)
     # Dimensional Ripper Yi Sang here
-    Dog1 = items("Telepole - Faust", "Faust", "HE", "EGO")
-    Bear = items("Hex Nail - Faust", "Faust", "TETH", "EGO")
-    Dog2 = items("Telepole - Don Quixote", "Don Quixote", "HE", "EGO")
-    God = items("Pursuance - Meursault", "Meursault", "HE", "EGO")
+    Dog1 = items("Telepole - Faust", "Faust", He, egotype)
+    Bear = items("Hex Nail - Faust", "Faust", Teth, egotype)
+    Dog2 = items("Telepole - Don Quixote", "Don Quixote", He, egotype)
+    God = items("Pursuance - Meursault", "Meursault", He, egotype)
     # Dimensional Ripper Hong Lu here
-    I_Am_Fiiire = items("Ardor Blossom Star - Ishmael", "Ishmael", "HE", "EGO")
-    Stew = items("Lifetime Stew - Sinclair", "Sinclair", "TETH", "EGO")
-    Nihil = items("Ya Sunyata Tad Rupam - Outis", "Outis", "TETH", "EGO")
-    Bad = items("Lantern - Gregor", "Gregor", "TETH", "EGO")
+    I_Am_Fiiire = items("Ardor Blossom Star - Ishmael", "Ishmael", He, egotype)
+    Stew = items("Lifetime Stew - Sinclair", "Sinclair", Teth, egotype)
+    Nihil = items("Ya Sunyata Tad Rupam - Outis", "Outis", Teth, egotype)
+    Bad = items("Lantern - Gregor", "Gregor", Teth, egotype)
     EGO = [
         Rocks,
         Dog1,
@@ -161,6 +170,7 @@ class MainSim:
         LCB_Sinner_Outis,
         LCB_Sinner_Rodya
     ]
+    # unused area for now, various information
     BPEGONumPaid = 5
     BPEGONumFree = 8
     BPEGONumSum = BPEGONumFree + BPEGONumPaid
@@ -175,19 +185,25 @@ class MainSim:
     # ways to make it actually draw the class instead of .name:
     # 1.somehow get the class not in a list and then print its name - convert single item list into that item
     # 2.namedtuple or something
+    # --------------------------------------
     def pullone(): # Standard extraction
-        pull = random.choices([MainSim.EGO[random.randint(0, 8)], MainSim.OOO[random.randint(0, 14)], MainSim.OO[random.randint(0, 19)], MainSim.O[random.randint(0, 11)]], [1.2996, 2.8995, 12.8, 83.0004], k=1)
+        pull = random.choices([MainSim.EGO[random.randint(0, 8)], MainSim.OOO[random.randint(0, 14)], MainSim.OO[random.randint(0, 19)], MainSim.O[random.randint(0, 11)]], 
+                              [1.2996, 2.8995, 12.8, 83.0004], k=1)
         drop = pull[0] # i was fucking stupid literally just take the index 0 oh my god
-        MainSim.results_str.append(drop.name)
+        MainSim.results.append(drop)
+        return drop.name, drop.rarity, drop.belongsTo, drop.type # for later
     #footnote: k is the amount of variables it wants to output
     def tenth(): # 10th pull 
-        pull = random.choices([MainSim.EGO[random.randint(0, 8)], MainSim.OOO[random.randint(0, 14)], MainSim.OO[random.randint(0, 19)]], [1.2996, 2.8995, 95.8009], k=1)
+        pull = random.choices([MainSim.EGO[random.randint(0, 8)], MainSim.OOO[random.randint(0, 14)], MainSim.OO[random.randint(0, 19)]], 
+                              [1.2996, 2.8995, 95.8009], k=1)
         drop = pull[0]
-        MainSim.results_str.append(drop.name)
+        MainSim.results.append(drop)
+        return drop.name, drop.rarity, drop.belongsTo, drop.type 
+    # --------------------------------------
     # testing area
-    def test1(): # testing lists
+    """ def test1(): # testing lists
         listhere = []
-        listhere.append(random.choices(["EGO", "OOO", "OO"], [1.2996, 2.8995, 95.8009], k=1))
+        listhere.append(random.choices([egotype, "OOO", "OO"], [1.2996, 2.8995, 95.8009], k=1))
         print(listhere)
     def test2(): # testing taking info from objects in a class
         ee = 0
@@ -197,13 +213,13 @@ class MainSim:
             ee += 1
         print(MainSim.results_str)
     def try1(): # just trying out
-        counterily = 10 # there is nothing wrong with this function. i fucked up the class assignment. FUN!!!! !   aaa
+        counterily = 10 # there is nothing wrong with this function. i fucked up the class assignment(items.name instead of self.name). FUN!!!! !  
         while counterily > 0:
             pulled = random.choices([MainSim.OO[random.randint(0, 19)].name, MainSim.O[random.randint(0, 11)].name], [12.8, 83.0004], k=1)
             MainSim.results_str.append(pulled)
             pulled = []
             counterily -= 1
-        print(MainSim.results_str)
+        print(MainSim.results_str) """
     # using the defined functions
     def draw10(count): # perform 9 1draws, and a special draw
         while count > 0: # the number of 10 extracts to sim
@@ -213,6 +229,10 @@ class MainSim:
                 pulls -= 1
             MainSim.tenth() # guaranteed 2star extraction
             count -= 1
+        draws = int(len(MainSim.results)) - 1
+        while draws > -1:
+            MainSim.results_str.append(MainSim.results[draws].name)
+            draws -= 1
         random.shuffle(MainSim.results_str)
         print(MainSim.results_str) 
     def draw1(count):
@@ -220,7 +240,7 @@ class MainSim:
             MainSim.pullone() # standard extraction for the first 9 times
             count -= 1
         print(MainSim.results_str)
-    def sorted10(count):
+    def sorted10(count): # need to sort by value items.rarity instead of .name alphabetical - maybe return values from pullone() or tenth()?
         while count > 0: # the number of 10 extracts to sim
             pulls = 10
             while pulls > 1:
@@ -228,6 +248,21 @@ class MainSim:
                 pulls -= 1
             MainSim.tenth() # guaranteed 2star extraction
             count -= 1
-        random.shuffle(MainSim.results_str)
+        draws = int(len(MainSim.results)) - 1 # how many items were drawn 
+        results1 = sorted(MainSim.results, key=lambda self: self.rarity, reverse=True)   # sort by rarity
+        while draws > -1:
+            MainSim.results_str.append(results1[draws].name)
+            draws -= 1 
         print(MainSim.results_str)
-    
+
+    # changes:
+    # updated tenth() to be in line with pullone() because that was directly appending into result_str and messing with sorted pulls
+    # 2 lists, one for the actual items and one for their names to be outputted 
+    # sort results_str by rarity
+    # actual words instead of just outputting pull results_str
+    # put the tenth pull somewhere random
+    # draw once
+
+# MainSim.sorted10(1) # sort by rarity
+# MainSim.draw10(1) # draw 10 randomly
+# MainSim.draw1(1) # draw 1 randomly
